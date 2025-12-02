aeb61e97fce2d894c794712e49e283566b67500f
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2025-12-02 12:47:14 +0100

    Fix duplicate output definitions in Terraform configuration

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -110,16 +110,6 @@ EOF
 /*------------------------------------------------
  Outputs
 ------------------------------------------------*/
-output "instance_ip" {
-  value = aws_instance.app_server.public_ip
-  description = "Public IP address of the EC2 instance"
-}
-
-output "public_ip" {
-  value = aws_instance.app_server.public_ip
-  description = "Public IP address of the EC2 instance"
-}
-
 output "instance_public_ip" {
   value = aws_instance.app_server.public_ip
   description = "Public IP address of the EC2 instance"
